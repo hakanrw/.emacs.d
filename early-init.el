c51035cb93dd7cf26ac1342c31fc0cdab552e8d2
Hakan Candar <hakancandar@proton.me>
2025-07-31 00:28:51 +0300

    initial emacs config

---
+++ early-init.el
@@ -0,0 +1,10 @@
+(setq load-path
+      (seq-remove (lambda (path)
+                    (string-prefix-p "/usr/share/emacs/site-lisp" path))
+                  load-path))
+
+(add-to-list 'default-frame-alist '(fullscreen . fullboth)) ;; or 'fullboth
+
+;; Suppress redisplay and messages early
+(setq-default inhibit-redisplay t
+              inhibit-message t)
---
+++ init.el
@@ -0,0 +1,16 @@
+;;(use-package benchmark-init
+;;  :ensure t
+;;  :init (benchmark-init/activate)
+;;  :hook (after-init . benchmark-init/deactivate))
+
+;;(load (expand-file-name "scripts/packages.el" user-emacs-directory))
+(load (expand-file-name "scripts/theme.el" user-emacs-directory))
+(load (expand-file-name "scripts/code.el" user-emacs-directory))
+(load (expand-file-name "scripts/shell.el" user-emacs-directory))
+(load (expand-file-name "scripts/dired.el" user-emacs-directory))
+(load (expand-file-name "scripts/vt.el" user-emacs-directory))
+(load (expand-file-name "scripts/tabs.el" user-emacs-directory))
+
+(setq inhibit-redisplay nil)
+;;(setq inhibit-message nil)
+

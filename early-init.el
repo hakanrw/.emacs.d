41330fd11179d4d80e6d868f8a538d41f81df90d
Candar <hakan.candar@siemens.com>
2025-08-04 08:06:01 +0300

    early initialization for smooth launch

--- early-init.el
+++ early-init.el
@@ -1,4 +1,16 @@
-(add-to-list 'default-frame-alist '(fullscreen . maximized)) ;; or 'fullboth
+(add-to-list 'default-frame-alist '(fullscreen . fullboth)) ;; or 'fullboth
+
+
+;; minimal early UI
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-startup-screen t)
+;;(set-frame-font "Monospace-12" nil t)
+(add-to-list 'default-frame-alist '(font . "Fira Code-10.5"))
+(load-theme 'wombat t) ;; or try 'tango-dark, 'deeper-blue, 'leuven for less 90s sadness
+(global-display-line-numbers-mode 1)
+
 
 ;; Suppress redisplay and messages early
 ;;(setq-default inhibit-redisplay t
